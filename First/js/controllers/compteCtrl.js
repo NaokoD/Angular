ea14a01552247551a090dd6d558707f5cf1925ed
NaokoD <ludovic.damprobe@gmail.com>
2016-11-30 15:34:05 +0100

    add

--- First/js/controllers/compteCtrl.js
+++ First/js/controllers/compteCtrl.js
@@ -1,14 +1,22 @@
 'use strict';
 
 myApp.controller('compteCtrl', function($scope, Comptes, compteService, $routeParams) {
-    $scope.comptes = {};
-    $scope.comptes = Comptes.comptes;
-    compteService.get($routeParams.id).then(function(data) {
-        $scope.data = data.liste;
-        console.log($scope.data);
-        for (let i = 0; i < $scope.data.length; i++) {
-            $scope.comptes.push($scope.data[i]);
-        }
-    });
+    $scope.comptes = [];
+    console.log("First Control");
+    console.log($scope.comptes);
+    console.log($routeParams.id);
+    if ($routeParams.id == 4){
+        $scope.comptes = Comptes.comptes;
+      }else{
+        compteService.get($routeParams.id).then(function(data) {
+            $scope.data = data.liste;
+            console.log($scope.data);
+            for (let i = 0; i < $scope.data.length; i++) {
+                $scope.comptes.push($scope.data[i]);
+            }
+        });
+      }
+    // $scope.comptes = Comptes.comptes;
+
     console.log($scope.comptes);
 });
